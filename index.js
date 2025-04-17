aeff587d60d73a8a64b2254f16b38e99e460691f
Sumaiya Sinja <sumiyasinja@gmail.com>
2025-04-17 21:39:25 +0600

    API added

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ctrkbrk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
  const client = new MongoClient(uri);
+ 
 
 async function run() {
   try {
@@ -29,7 +30,7 @@ async function run() {
 
     // Equipment API
     app.get("/equipments", async (req, res) => {
-      const cursor = equipmentCollection.find({});
+      const cursor = equipmentCollection.find();
       const equipments = await cursor.toArray();
       res.send(equipments);
     });
@@ -42,14 +43,14 @@ async function run() {
 
     app.delete("/equipments/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: ObjectId(id) };
+      const query = { _id: new ObjectId(id) };
       const result = await equipmentCollection.deleteOne(query);
       res.send(result);
     });
 
     app.get("/equipments/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: ObjectId(id) };
+      const query = { _id: new  ObjectId(id) };
       const equipment = await equipmentCollection.findOne(query);
       res.send(equipment);
     });
@@ -57,7 +58,7 @@ async function run() {
     app.put("/equipments/:id", async (req, res) => {
       const id = req.params.id;
       const equipment = req.body;
-      const filter = { _id: ObjectId(id) };
+      const filter = { _id:  new ObjectId(id) };
       const options = { upsert: true };
       const updateDoc = {
         $set: {
@@ -71,6 +72,23 @@ async function run() {
       );
       res.send(result);
     });
+
+// Fetch equipments by seller email
+app.get("/equipments/by-email/:email", async (req, res) => {
+  const email = req.params.email;
+  const query = { seller_email: email };
+  const equipment = await equipmentCollection.find(query).toArray();
+  res.send(equipment);
+});
+
+// Fetch equipments by category name
+app.get("/equipments/by-category/:category", async (req, res) => {
+  const category = req.params.category;
+  const query = { categoryName: category };
+  const equipment = await equipmentCollection.find(query).toArray();
+  res.send(equipment);
+});
+
     
 
   } finally {
