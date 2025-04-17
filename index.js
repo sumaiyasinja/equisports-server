a8e417bd7578391043e77ef7435fa75f51231e37
Sumaiya Sinja <sumiyasinja@gmail.com>
2025-04-18 02:05:57 +0600

    bug fixed

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ctrkbrk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
- const client = new MongoClient(uri);
- 
+const client = new MongoClient(uri);
 
 async function run() {
   try {
@@ -24,8 +22,8 @@ async function run() {
     // User API
     app.post("/users", async (req, res) => {
       const user = req.body;
-        const result = await userCollection.insertOne(user);
-        res.send(result);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
     });
 
     // Equipment API
@@ -50,7 +48,7 @@ async function run() {
 
     app.get("/equipments/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new  ObjectId(id) };
+      const query = { _id: new ObjectId(id) };
       const equipment = await equipmentCollection.findOne(query);
       res.send(equipment);
     });
@@ -58,49 +56,58 @@ async function run() {
     app.put("/equipments/:id", async (req, res) => {
       const id = req.params.id;
       const equipment = req.body;
-      const filter = { _id:  new ObjectId(id) };
+      console.log("Body", equipment, id);
+    
+      const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
+    
       const updateDoc = {
         $set: {
-          equipment,
+          image: equipment?.image,
+          itemName: equipment?.itemName,
+          categoryName: equipment?.categoryName,
+          description: equipment?.description,
+          price: equipment?.price,
+          rating: equipment?.rating,
+          customization: equipment?.customization,
+          processingTime: equipment?.processingTime,
+          stockStatus: equipment?.stockStatus,
+          
         },
       };
-      const result = await equipmentCollection.updateOne(
-        filter,
-        updateDoc,
-        options
-      );
+    
+      console.log("updateDoc", updateDoc);
+    
+      const result = await equipmentCollection.updateOne(filter, updateDoc, options);
       res.send(result);
     });
-
-// Fetch equipments by seller email
-app.get("/equipments/by-email/:email", async (req, res) => {
-  const email = req.params.email;
-  const query = { seller_email: email };
-  const equipment = await equipmentCollection.find(query).toArray();
-  res.send(equipment);
-});
-
-// Fetch equipments by category name
-app.get("/equipments/by-category/:category", async (req, res) => {
-  const category = req.params.category;
-  const query = { categoryName: category };
-  const equipment = await equipmentCollection.find(query).toArray();
-  res.send(equipment);
-});
-
     
 
+    // Fetch equipments by seller email
+    app.get("/equipments/by-email/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { seller_email: email };
+      const equipment = await equipmentCollection.find(query).toArray();
+      res.send(equipment);
+    });
+
+    // Fetch equipments by category name
+    app.get("/equipments/by-category/:category", async (req, res) => {
+      const category = req.params.category;
+      const query = { categoryName: category };
+      const equipment = await equipmentCollection.find(query).toArray();
+      res.send(equipment);
+    });
   } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello Athletes!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello Athletes!");
+});
 
 app.listen(port, () => {
-  console.log(`Sport Equipment Server listening on port ${port}`)
-})
+  console.log(`Sport Equipment Server listening on port ${port}`);
+});
